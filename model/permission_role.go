840a12372ecf0298071ad93ff75987f632400116
godcong <jumbycc@163.com>
2019-01-27 15:49:16 +0800

    update manager

--- model/permission_role.go
+++ model/permission_role.go
@@ -1,1 +1,9 @@
 package model
+
+import "github.com/google/uuid"
+
+// PermissionRole ...
+type PermissionRole struct {
+	PermissionID uuid.UUID `json:"permission_id" xorm:"permission_id uuid"`
+	RoleID       uuid.UUID `json:"role_id" xorm:"role_id uuid"`
+}
--- model/permission_user.go
+++ model/permission_user.go
@@ -1,1 +1,9 @@
 package model
+
+import "github.com/google/uuid"
+
+// PermissionUser ...
+type PermissionUser struct {
+	PermissionID uuid.UUID `json:"permission_id" xorm:"permission_id uuid"`
+	UserID       uuid.UUID `json:"user_id" xorm:"user_id uuid"`
+}
