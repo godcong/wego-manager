a333043e0c73e9b63a908cdbe6aa1d0a486d805c
godcong <jumbycc@163.com>
2019-01-29 01:03:44 +0800

    update manager

--- model/role_user.go
+++ model/role_user.go
@@ -1,8 +1,36 @@
 package model
 
+import "golang.org/x/exp/xerrors"
+
 // RoleUser ...
 type RoleUser struct {
 	Model  `xorm:"-"`
 	RoleID string `json:"role_id" xorm:"role_id notnull uuid"`
 	UserID string `json:"user_id" xorm:"user_id notnull uuid"`
+}
+
+// Relate ...
+func (obj *RoleUser) Relate() (*Role, *User, error) {
+	var info = new([]struct {
+		Role Role `xorm:"extends"`
+		User User `xorm:"extends"`
+	})
+	session := DB().Table(&Role{}).Select("role.*, user.*").
+		Join("left", obj, "role_user.role_id = role.id").
+		Join("left", &User{}, "role_user.user_id = user.id")
+
+	if obj.UserID != "" {
+		session = session.Where("user.id = ? ", obj.UserID)
+	}
+	if obj.RoleID != "" {
+		session = session.Where("role.id = ? ", obj.RoleID)
+	}
+	i, err := session.FindAndCount(info)
+	if err != nil {
+		return nil, nil, xerrors.Errorf("relate: %w", err)
+	}
+	if i > 1 {
+		return nil, nil, xerrors.Errorf("count %d > 1 ", i)
+	}
+	return &(*info)[0].Role, &(*info)[0].User, nil
 }
