9a130a261a244cb8a6a449ad8b49725d5c26d389
godcong <jumbycc@163.com>
2019-01-29 01:02:11 +0800

    update manager

--- model/permission_role.go
+++ model/permission_role.go
@@ -1,8 +1,36 @@
 package model
 
+import "golang.org/x/exp/xerrors"
+
 // PermissionRole ...
 type PermissionRole struct {
 	Model        `xorm:"-"`
 	PermissionID string `json:"permission_id" xorm:"permission_id notnull uuid"`
 	RoleID       string `json:"role_id" xorm:"role_id notnull uuid"`
+}
+
+// Relate ...
+func (obj *PermissionRole) Relate() (*Permission, *Role, error) {
+	var info = new([]struct {
+		Permission Permission `xorm:"extends"`
+		Role       Role       `xorm:"extends"`
+	})
+	session := DB().Table(&Permission{}).Select("permission.*, role.*").
+		Join("left", obj, "permission_role.permission_id = permission.id").
+		Join("left", &Role{}, "permission_role.role_id = role.id")
+
+	if obj.UserID != "" {
+		session = session.Where("role.id = ? ", obj.RoleID)
+	}
+	if obj.PermissionID != "" {
+		session = session.Where("permission.id = ? ", obj.PermissionID)
+	}
+	i, err := session.FindAndCount(info)
+	if err != nil {
+		return nil, nil, xerrors.Errorf("relate: %w", err)
+	}
+	if i > 1 {
+		return nil, nil, xerrors.Errorf("count %d > 1 ", i)
+	}
+	return &(*info)[0].Permission, &(*info)[0].Role, nil
 }
