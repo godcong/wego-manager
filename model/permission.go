158c15fd2e761cd9339a57768fbcb5e1e3cdb734
godcong <jumbycc@163.com>
2019-01-31 23:58:20 +0800

    update manager

--- model/permission.go
+++ model/permission.go
@@ -1,5 +1,7 @@
 package model
 
+import "golang.org/x/exp/xerrors"
+
 // Permission ...
 type Permission struct {
 	Model           `xorm:"extends"`
@@ -16,4 +18,60 @@ func NewPermission(id string) *Permission {
 			ID: id,
 		},
 	}
+}
+
+// Get ...
+func (obj *Permission) Get() (bool, error) {
+	return Get(nil, obj)
+}
+
+// Update ...
+func (obj *Permission) Update(cols ...string) (int64, error) {
+	return Update(nil, obj.ID, obj)
+}
+
+// Roles ...
+func (obj *Permission) Permissions() ([]*Permission, error) {
+	var permissions []*Permission
+	err := DB().Table(obj).Find(&permissions)
+	if err != nil {
+		return nil, xerrors.Errorf("find: %w", err)
+	}
+	return permissions, nil
+}
+
+// Roles ...
+func (obj *Permission) Roles() ([]*Role, error) {
+	var roles []*Role
+	session := DB().Table(&Role{}).Select("role.*").
+		Join("left", &PermissionRole{}, "permission_role.role_id = role.id")
+
+	if obj.ID != "" {
+		session = session.Where("permission_role.permission_id = ? ", obj.ID)
+	}
+
+	err := session.Find(&roles)
+	if err != nil {
+		return nil, xerrors.Errorf("relate: %w", err)
+	}
+
+	return roles, nil
+}
+
+// Users ...
+func (obj *Permission) Users() ([]*User, error) {
+	var users []*User
+	session := DB().Table(User{}).Select("user.*").
+		Join("left", &RoleUser{}, "permission_user.user_id = user.id")
+
+	if obj.ID != "" {
+		session = session.Where("user.id = ? ", obj.ID)
+	}
+
+	err := session.Find(&users)
+	if err != nil {
+		return nil, xerrors.Errorf("relate: %w", err)
+	}
+
+	return users, nil
 }
