60ea6f8e6c34c96cfefeb1839c936d793b01e460
godcong <jumbycc@163.com>
2019-02-01 23:50:03 +0800

    update manager

---
+++ util/token.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"github.com/json-iterator/go"
+	"log"
+	"time"
+)
+
+// WebToken ...
+type WebToken struct {
+	UID           string `json:"oid"`
+	Username      string `json:"username"`
+	Nickname      string `json:"nickname"`
+	EffectiveTime int64  `json:"effectiveTime"`
+}
+
+// NewWebToken ...
+func NewWebToken(uid string) *WebToken {
+	return &WebToken{
+		UID:           uid,
+		EffectiveTime: time.Now().Unix() + 3600*24*7,
+	}
+}
+
+// ToToken ...
+func ToToken(key string, token *WebToken) (string, error) {
+	sub, err := jsoniter.Marshal(token)
+	if err != nil {
+		return "", err
+	}
+	jwt, err := EncryptJWT([]byte(key), sub)
+	return jwt, err
+}
+
+// FromToken ...
+func FromToken(key, token string) (*WebToken, error) {
+	t := WebToken{}
+	sub, err := DecryptJWT([]byte(key), token)
+	log.Println("sub", sub)
+	if err != nil {
+		return nil, err
+	}
+
+	err = jsoniter.Unmarshal([]byte(sub), &t)
+	if err != nil {
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &t, nil
+}
