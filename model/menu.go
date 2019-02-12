2f3d8b69edc90315fc39cbea7062b0af0ffe2bdd
godcong <jumbycc@163.com>
2019-02-12 16:09:15 +0800

    update manager

---
+++ model/menu.go
@@ -0,0 +1,13 @@
+package model
+
+type Menu struct {
+	Model       `xorm:"extends"`
+	PID         string `xorm:"pid"` //parent id
+	Name        string `xorm:"name"`
+	Icon        string `xorm:"icon"`
+	Slug        string `xorm:"slug"`
+	URL         string `xorm:"url"`
+	Active      string `xorm:"active"`
+	Description string `xorm:"description"`
+	Sort        int    `xorm:"sort"`
+}
