58e49df7782991ed2d7e13181e775befdcd0d74a
godcong <jumbycc@163.com>
2019-02-12 17:54:56 +0800

    update manager

--- middleware/csrf.go
+++ middleware/csrf.go
@@ -2,17 +2,15 @@ package middleware
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/nosurf"
 	"net/http"
 )
 
 func CSRF(ver string) {
 	//TODO:
-	csrf := nosurf.New(gin.Default())
-	csrf.SetFailureHandler(http.HandlerFunc(csrfFailHandler))
-
-	http.ListenAndServe(":8000", csrf)
+	//csrf := nosurf.New(gin.Default())
+	//csrf.SetFailureHandler(http.HandlerFunc(csrfFailHandler))
+	//http.ListenAndServe(":8000", csrf)
 }
 
 func csrfFailHandler(w http.ResponseWriter, r *http.Request) {
