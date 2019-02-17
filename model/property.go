c1cc8f50f897726ca19d528fd555b59ea2ac67c6
godcong <jumbycc@163.com>
2019-02-18 00:37:52 +0800

    update manager

--- model/property.go
+++ model/property.go
@@ -1,6 +1,8 @@
 package model
 
-import "golang.org/x/xerrors"
+import (
+	"golang.org/x/xerrors"
+)
 
 // OAuth ...
 type OAuth struct {
@@ -13,23 +15,23 @@ type Property struct {
 	Model       `xorm:"extends" json:",inline"`
 	UserID      string   `xorm:"user_id" json:"user_id"`
 	Sign        string   `xorm:"sign" json:"sign"`
-	AppID       string   `xorm:"app_id notnull unique" json:"app_id"`
+	AppID       string   `xorm:"unique app_id " json:"app_id"`
 	MchID       string   `xorm:"mch_id" json:"mch_id"`
 	MchKey      string   `xorm:"mch_key" json:"mch_key"`
-	Cert        string   `xorm:"varchar(2048) cert" json:"cert"`
-	Key         string   `xorm:"varchar(2048) key" json:"key"`
-	RootCA      string   `xorm:"varchar(2048) root_ca" json:"root_ca"`
+	PemCert     string   `xorm:"pem_cert" json:"pem_cert"`
+	PemKEY      string   `xorm:"pem_key" json:"pem_key"`
+	RootCA      string   `xorm:"root_ca" json:"root_ca"`
 	NotifyURL   string   `xorm:"notify_url" json:"notify_url"`
 	RefundURL   string   `xorm:"refund_url" json:"refund_url"`
 	Kind        string   `xorm:"kind" json:"kind"`
-	Sandbox     bool     `xorm:"sandbox" json:"sandbox" json:"sandbox"`
-	Secret      string   `xorm:"secret" json:"secret"`
+	Sandbox     bool     `xorm:"sandbox" json:"sandbox" `
+	AppSecret   string   `xorm:"app_secret" json:"app_secret"`
 	Token       string   `xorm:"token" json:"token"`
 	AesKey      string   `xorm:"aes_key" json:"aes_key"`
 	PublicKey   string   `xorm:"public_key" json:"public_key"`
 	PrivateKey  string   `xorm:"private_key" json:"private_key"`
 	Scopes      []string `xorm:"scopes" json:"scopes"`
-	RedirectURL string   `xorm:"redirect_url" json:"redirect_url"`
+	RedirectURI string   `xorm:"redirect_url" json:"redirect_url"`
 }
 
 // NewProperty ...
