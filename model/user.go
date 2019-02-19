e281c3af849506d9e08508d23ccb6513891ebcdb
godcong <jumbycc@163.com>
2019-02-19 23:49:06 +0800

    update manager

--- model/user.go
+++ model/user.go
@@ -7,6 +7,15 @@ import (
 	"net/url"
 )
 
+// UserType ...
+type UserType string
+
+// UserTypeAdmin ...
+const UserTypeAdmin UserType = "admin"
+
+// UserTypeUser ...
+const UserTypeUser UserType = "user"
+
 // Login ...
 type Login struct {
 	Username string `json:"username" ` //用户名
@@ -15,22 +24,21 @@ type Login struct {
 
 // User ...
 type User struct {
-	Model         `xorm:"extends" json:",inline"`
-	WechatUserID  string `json:"wechat_user_id" xorm:"wechat_user_id comment(微信用户ID)"`
-	Enable        bool   `json:"enable" xorm:"enable"`                    //是否启用
-	Nickname      string `json:"nickname" xorm:"nickname"`                //名称
-	Username      string `json:"username" xorm:"username notnull unique"` //用户名
-	Email         string `json:"email" xorm:"email"`                      //邮件
-	Mobile        string `json:"mobile" xorm:"mobile"`                    //移动电话
-	IDCardFacade  string `json:"id_card_facade" xorm:"id_card_facade"`    //身份证(正)
-	IDCardObverse string `json:"id_card_obverse" xorm:"id_card_obverse"`  //身份证(反)
-	Password      string `json:"password" xorm:"password"`                //密码
-	Certificate   string `json:"certificate" xorm:"certificate"`          //证书
-	PrivateKey    string `json:"private_key" xorm:"private_key"`          //私钥
-	LoginIP       string `json:"login_ip" xorm:"login_ip"`                //本次登录IP
-	Token         string `json:"-" xorm:"varchar(1024) token"`            //Token
-	Salt          string `json:"-" xorm:"slat"`                           //盐值
-	Sign          string `json:"-" xorm:"sign"`                           //外调值
+	Model        `xorm:"extends" json:",inline"`
+	WechatUserID string   `json:"wechat_user_id" xorm:"wechat_user_id comment(微信用户ID)"`
+	Enable       bool     `json:"enable" xorm:"enable"`                                //是否启用
+	UserType     UserType `json:"user_type" xorm:"user_type"`                          //用户类型
+	Nickname     string   `json:"nickname" xorm:"nickname"`                            //名称
+	Username     string   `json:"username" xorm:"username notnull default('') unique"` //用户名
+	Email        string   `json:"email" xorm:"email notnull default('')"`              //邮件
+	Mobile       string   `json:"mobile" xorm:"mobile notnull default('')"`            //移动电话
+	Password     string   `json:"password" xorm:"password"`                            //密码
+	Certificate  string   `json:"certificate" xorm:"certificate"`                      //证书
+	PrivateKey   string   `json:"private_key" xorm:"private_key"`                      //私钥
+	LoginIP      string   `json:"login_ip" xorm:"login_ip"`                            //本次登录IP
+	Sign         string   `json:"sign" xorm:"notnull unique default('')  comment(分享码)"`
+	Token        string   `json:"-" xorm:"varchar(1024) token"` //Token
+	Salt         string   `json:"-" xorm:"slat"`                //盐值
 }
 
 // NewUser ...
@@ -63,59 +71,6 @@ func (obj *User) Users() ([]*User, error) {
 		return nil, xerrors.Errorf("find: %w", err)
 	}
 	return users, nil
-}
-
-// Permissions ...
-func (obj *User) Permissions() ([]*Permission, error) {
-	var permissions []*Permission
-	session := DB().Table(&Permission{}).Select("permission.*").
-		Join("left", &PermissionUser{}, "permission_user.permission_id = permission.id")
-
-	if obj.ID != "" {
-		session = session.Where("permission_user.user_id = ? ", obj.ID)
-	}
-
-	err := session.Find(&permissions)
-	if err != nil {
-		return nil, xerrors.Errorf("relate: %w", err)
-	}
-
-	return permissions, nil
-}
-
-// CheckPermission ...
-func (obj *User) CheckPermission(funcName string) bool {
-	var permissions []*Permission
-	session := DB().Table(&Permission{}).Select("permission.*").
-		Join("left", &PermissionUser{}, "permission_user.permission_id = permission.id").
-		Where("permission.slug = ?", funcName)
-	if obj.ID != "" {
-		session = session.Where("permission_user.user_id = ? ", obj.ID)
-	}
-
-	i, err := session.FindAndCount(&permissions)
-	if err != nil || i <= 0 {
-		return false
-	}
-	return true
-}
-
-// Roles ...
-func (obj *User) Roles() ([]*Role, error) {
-	var roles []*Role
-	session := DB().Table(&Role{}).Select("role.*").
-		Join("left", &RoleUser{}, "role_user.role_id = role.id")
-
-	if obj.ID != "" {
-		session = session.Where("role_user.user_id = ? ", obj.ID)
-	}
-
-	err := session.Find(&roles)
-	if err != nil {
-		return nil, xerrors.Errorf("relate: %w", err)
-	}
-
-	return roles, nil
 }
 
 // Validate ...
---
+++ model/user_infomation.go
@@ -0,0 +1,15 @@
+package model
+
+// UserInformation ...
+type UserInformation struct {
+	Model         `xorm:"extends" json:",inline"`
+	UserID        string `xorm:"notnull default('') unique user_id" json:"user_id"`
+	Email         string `json:"email" xorm:"email"`                     //邮件
+	Mobile1       string `json:"mobile" xorm:"mobile1"`                  //移动电话
+	Mobile2       string `json:"mobile" xorm:"mobile2"`                  //移动电话
+	Mobile3       string `json:"mobile" xorm:"mobile3"`                  //移动电话
+	Mobile4       string `json:"mobile" xorm:"mobile4"`                  //移动电话
+	Mobile5       string `json:"mobile" xorm:"mobile5"`                  //移动电话
+	IDCardFacade  string `json:"id_card_facade" xorm:"id_card_facade"`   //身份证(正)
+	IDCardObverse string `json:"id_card_obverse" xorm:"id_card_obverse"` //身份证(反)
+}
