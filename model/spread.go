aa589022cfa3cf845517d4570a0217c2016f6452
godcong <jumbycc@163.com>
2019-02-24 05:44:31 +0800

    update manager

--- model/spread.go
+++ model/spread.go
@@ -4,17 +4,17 @@ package model
 type Spread struct {
 	Model `xorm:"extends"`
 	//ActivityID    string `json:"activity_id"`
-	Code          string `json:"code"`
-	UserID        string `json:"user_id"`
-	ParentUserID1 string `json:"parent_user_id_1 comment(上1级)"`
-	ParentUserID2 string `json:"parent_user_id_2 comment(上2级)"`
-	ParentUserID3 string `json:"parent_user_id_3 comment(上3级)"`
-	ParentUserID4 string `json:"parent_user_id_4 comment(上4级)"`
-	ParentUserID5 string `json:"parent_user_id_5 comment(上5级)"`
-	ParentUserID6 string `json:"parent_user_id_6 comment(上6级)"`
-	ParentUserID7 string `json:"parent_user_id_7 comment(上7级)"`
-	ParentUserID8 string `json:"parent_user_id_8 comment(上8级)"`
-	ParentUserID9 string `json:"parent_user_id_9 comment(上9级)"`
+	Code          string `json:"notnull default('') code" json:"code"`
+	UserID        string `json:"notnull default('') user_id" json:"user_id"`
+	ParentUserID1 string `json:"notnull default('') parent_user_id_1" json:"parent_user_id_1 comment(上1级)"`
+	ParentUserID2 string `json:"notnull default('') parent_user_id_2" json:"parent_user_id_2 comment(上2级)"`
+	ParentUserID3 string `json:"notnull default('') parent_user_id_3" json:"parent_user_id_3 comment(上3级)"`
+	ParentUserID4 string `json:"notnull default('') parent_user_id_4" json:"parent_user_id_4 comment(上4级)"`
+	ParentUserID5 string `json:"notnull default('') parent_user_id_5" json:"parent_user_id_5 comment(上5级)"`
+	ParentUserID6 string `json:"notnull default('') parent_user_id_6" json:"parent_user_id_6 comment(上6级)"`
+	ParentUserID7 string `json:"notnull default('') parent_user_id_7" json:"parent_user_id_7 comment(上7级)"`
+	ParentUserID8 string `json:"notnull default('') parent_user_id_8" json:"parent_user_id_8 comment(上8级)"`
+	ParentUserID9 string `json:"notnull default('') parent_user_id_9" json:"parent_user_id_9 comment(上9级)"`
 }
 
 // NewSpread ...
