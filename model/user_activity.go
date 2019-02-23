a58d38182f10737c087a53021080cc9efd3bc4a3
godcong <jumbycc@163.com>
2019-02-24 03:56:15 +0800

    update manager

--- model/user_activity.go
+++ model/user_activity.go
@@ -5,6 +5,7 @@ import "golang.org/x/xerrors"
 // UserActivity ...
 type UserActivity struct {
 	Model        `xorm:"extends" json:",inline"`
+	PropertyID   string `xorm:"notnull default('') comment(配置ID) property_id" json:"property_id"`
 	ActivityID   string `xorm:"notnull unique(user_activity) default('') comment(活动ID) activity_id" json:"activity_id"`
 	UserID       string `xorm:"notnull unique(user_activity) default('') comment(参加活动的用户ID) user_id" json:"user_id"`
 	SpreadCode   string `xorm:"notnull unique default('') comment(参加活动的用户推广码) spread_code"  json:"spread_code"`
@@ -43,4 +44,24 @@ func (obj *UserActivity) CodeSpread() (*Spread, error) {
 	*obj = info.UserActivity
 
 	return &info.Spread, nil
+}
+
+// Property ...
+func (obj *UserActivity) Property() (*Property, error) {
+	var info struct {
+		UserActivity UserActivity `xorm:"extends"`
+		Property     Property     `xorm:"extends"`
+	}
+	b, e := DB().Table(obj).Join("left", info.Property, "user_activity.property_id = property.id").
+		Where("user_activity.id = ?", obj.ID).Get(&info)
+	if e != nil {
+		return nil, e
+	}
+	if !b {
+		e = xerrors.New("property not found")
+		return nil, e
+	}
+	*obj = info.UserActivity
+
+	return &info.Property, nil
 }
