dd382d0773f3d8fde5a585943c828a73a66e671b
godcong <jumbycc@163.com>
2019-02-27 15:32:05 +0800

    update wego-auth-manager

--- model/module.go
+++ model/module.go
@@ -4,6 +4,7 @@ package model
 type Module struct {
 	Model  `xorm:"extends" json:",inline"`
 	Name   string `xorm:"name" json:"name"`
+	Alias  string `xorm:"alias" json:"alias"`
 	Site   string `xorm:"site" json:"site"`
 	Deploy string `xorm:"deploy" json:"deploy"`
 	Log    string `xorm:"log" json:"log"`
