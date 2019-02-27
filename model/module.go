f9e0e609f02d40433cbe7d98705ee2839de60df1
godcong <jumbycc@163.com>
2019-02-27 15:36:56 +0800

    update wego-auth-manager

--- model/module.go
+++ model/module.go
@@ -2,12 +2,13 @@ package model
 
 // Module ...
 type Module struct {
-	Model  `xorm:"extends" json:",inline"`
-	Name   string `xorm:"name" json:"name"`
-	Alias  string `xorm:"alias" json:"alias"`
-	Site   string `xorm:"site" json:"site"`
-	Deploy string `xorm:"deploy" json:"deploy"`
-	Log    string `xorm:"log" json:"log"`
+	Model   `xorm:"extends" json:",inline"`
+	Name    string   `xorm:"notnull default('') name" json:"name"`   //模块名称
+	Alias   string   `xorm:"notnull default('') alias" json:"alias"` //模块别名url+别名+process
+	Site    string   `xorm:"notnull default('') site" json:"site"`
+	Deploy  string   `xorm:"notnull default('') deploy" json:"deploy"`
+	Log     string   `xorm:"notnull default('') log" json:"log"`
+	Depends []string `xorm:"depends" json:"depends"`
 }
 
 // NewModule ...
