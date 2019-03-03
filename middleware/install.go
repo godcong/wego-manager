6a0828644490bd0390575f2992b443247cacb27d
godcong <jumbycc@163.com>
2019-03-03 23:58:02 +0800

    update manager

--- middleware/install.go
+++ middleware/install.go
@@ -5,8 +5,8 @@ import (
 	"github.com/godcong/wego-auth-manager/config"
 )
 
-// Install ...
-func Install(ver string) gin.HandlerFunc {
+// InstallCheck ...
+func InstallCheck(ver string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if !config.IsInitialized() {
 			//config.InitConfig()
