8e76c78194906d7e06ae9d52258676a7f19b8a3e
godcong <jumbycc@163.com>
2019-03-04 00:04:33 +0800

    update manager

--- model/activity.go
+++ model/activity.go
@@ -9,11 +9,11 @@ type Activity struct {
 	Model      `xorm:"extends" json:",inline"`
 	UserID     string `xorm:"notnull default('') comment(创建活动的用户ID) user_id" json:"user_id"`
 	PropertyID string `xorm:"notnull default('') comment(配置ID) property_id" json:"property_id"`
-	Name       string `xorm:"notnull default('') comment(名称) name" json:"name"`            //活动名称
-	Code       string `xorm:"notnull unique default('') comment(活动码) code" json:"code"`    //活动码
-	Public     string `xorm:"notnull unique default('') comment(公开) public" json:"public"` //是否公开
-	Verify     bool   `xorm:"notnull default(false) comment(是否校验) verify" json:"verify"`   //是否校验
-	Mode       string `xorm:"notnull default('') comment(活动模式) mode" json:"mode"`          //活动模式
+	Name       string `xorm:"notnull default('') comment(名称) name" json:"name"`                //活动名称
+	Code       string `xorm:"notnull unique default('') comment(活动码) code" json:"code"`        //活动码
+	IsPublic   bool   `xorm:"notnull default(false) comment(公开) is_public" json:"is_public"`   //是否公开
+	IsVerify   bool   `xorm:"notnull default(false) comment(是否校验) is_verify" json:"is_verify"` //是否校验
+	Mode       string `xorm:"notnull default('') comment(活动模式) mode" json:"mode"`              //活动模式
 }
 
 // NewActivity ...
